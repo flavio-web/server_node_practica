d8660d593fe0385e83b90e3669134a6b0cb15def
Flavio Roman <flavioromanweb@gmail.com>
2024-05-31 22:24:04 -0500

    ejercicio terminao

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', ( req, res )=>{
+app.use( express.json() );
+app.use( express.static('public') );
+
+
+app.get('/', (req, res) =>{
+
+    res.sendFile( __dirname +  '/public/index.html');
+});
+
+
+app.get('/usuario', ( req, res )=>{
 
     const usuario = {
         nombre: 'Maria',
@@ -12,6 +22,30 @@ app.get('/', ( req, res )=>{
 
     res.json( usuario );
 });
+
+
+app.get('/:id', (req, res) =>{
+    const { id } = req.params;
+    res.json({ id });
+});
+
+app.post('/', ( req, res ) =>{
+    const data = req.body;
+    console.log( data );
+
+    res.json({ ruta: 'Metodo Post', data });
+});
+
+app.put('/', ( req, res )=>{
+    const data = req.body;
+    res.json({ ruta: 'Metodo Put', data });
+});
+
+app.delete('/:id', (req, res) =>{
+    const id = req.params.id;
+    res.json({ ruta: 'Metodo Delete', id });
+});
+
 
 app.get('*', ( req, res ) =>{
     const pagina = {
